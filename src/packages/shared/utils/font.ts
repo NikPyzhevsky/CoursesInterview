6b206f14c55508f9b74ddfd5df7b0ccece320f4e
Mikita <nikpyzh5@gmail.com>
2025-07-21 23:59:19 +0300

    feat: font

--- src/packages/shared/utils/font.ts
+++ src/packages/shared/utils/font.ts
@@ -23,9 +23,9 @@ export type Typography =
   | 'Caption.M.B';
 
 export type FontWeight = '400' | '500' | '600' | '700' | '800';
-type FontFamily = 'Inter';
+type FontFamily = 'Inter' | 'Nunito';
 
-const sourceBodyFont = Platform.OS === 'ios' ? 'Inter' : 'Inter';
+const sourceBodyFont = Platform.OS === 'ios' ? 'Nunito' : 'Nunito';
 const sourceHeaderFont = Platform.OS === 'ios' ? 'Inter' : 'Inter';
 
 export const getFontStyleByWeight = (
@@ -81,8 +81,8 @@ export const FONT_VARIANTS = {
   M: getFontStyleByWeight('400', 'Inter', 16, 22.4),
   'M.Bold': getFontStyleByWeight('700', 'Inter', 16, 22.4),
   'M.SemiBold': getFontStyleByWeight('600', 'Inter', 16, 22.4),
-  S: getFontStyleByWeight('400', 'Inter', 14, 20),
-  'S.Bold': getFontStyleByWeight('700', 'Inter', 14, 20),
-  'S.SemiBold': getFontStyleByWeight('600', 'Inter', 14, 20),
-  'S.ExtraBold': getFontStyleByWeight('800', 'Inter', 14, 20),
+  S: getFontStyleByWeight('400', 'Nunito', 14, 20),
+  'S.Bold': getFontStyleByWeight('700', 'Nunito', 14, 20),
+  'S.SemiBold': getFontStyleByWeight('600', 'Nunito', 14, 20),
+  'S.ExtraBold': getFontStyleByWeight('800', 'Nunito', 14, 20),
 } satisfies Record<Typography, TextStyle>;
